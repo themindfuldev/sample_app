178adfc2dca552b63dfd816ae09207f6932daede
Tiago Garcia <tiagoromerogarcia@gmail.com>
2013-05-15 21:33:15 -0300

    sign in/sign out feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -11,4 +12,8 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+
+  def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+  end  
 end
